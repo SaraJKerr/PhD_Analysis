75bc443cd1caa88c36c39440bb2e95a6ec2c70bb
SaraJKerr <sarajkerr@me.com>
2017-11-03 10:54:46 +0000

    updated code

--- Text_cleaning.R
+++ Text_cleaning.R
@@ -13,7 +13,7 @@
 # rescanned using Abbyy FineReader there are a number of OCR errors which 
 # remain - the majority of these are page numbers and random letters used as
 # notes for the bookbinders. This script carries out minimal cleaning by 
-# removing individual letters (with the exception of a, i, m, t, A and I), and
+# removing individual letters (with the exception of a, i, A and I), and
 # digits. The files are then resaved.
 
 # To remove random letters and page numbers from Owenson
@@ -22,11 +22,16 @@ input.dir <- "so_corpus"
 # Read the name of all .txt files
 files <- dir(input.dir, "\\.txt") 
 
+setwd("~/PhD_Main/PhD_Analysis/4_1/so_corpus")
 
 x <- scan(file = files[6], what = "char", 
                   sep = "\n", quote = "", comment.char = "")
-x <- gsub(" *\\b[^aimtAI]{1}\\b *", " ", x) # Remove random letters
+x <- gsub(" *\\b[^aiAI]{1}\\b *", " ", x) # Remove random letters
 x <- gsub("\\d", "", x) # Remove numbers
 write(x, paste0("clean/", files[6]))
 
-
+# To remove headers
+x <- scan(file = files[2], what = "char", 
+          sep = "\n", quote = "", comment.char = "")
+x <- gsub("DOMINICK", "", x) # Specific capitalised words
+write(x, paste0("clean/", files[2]))
