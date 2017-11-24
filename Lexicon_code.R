dfaffce62e9931a42d83a068fbd678307eb64d69
SaraJKerr <sarajkerr@me.com>
2017-11-24 16:56:13 +0000

    updated code

--- Lexicon_code.R
+++ Lexicon_code.R
@@ -8,23 +8,51 @@
 # Purpose: Code to create a lexicon of terms related to independence           #
 # Data Used: clmet3.1 corpus                                                   #
 # Packages Used:  wordVectors                                                  #
-# Last Updated: 8 November 2017                                                #
+# Last Updated: 24 November 2017                                                #
 ################################################################################
 
-# Creating a lexi independence collection of terms
+# Creating a lexicon of independence terms
 
 setwd("~/PhD_Main/PhD_Analysis/Data/clmet/corpus/txt")
 
 library(wordVectors)
 
-prep_word2vec("plain", "Clmet_processed.txt", lowercase = T)
+prep_word2vec("plain", "Clmet_processed_period_1_2.txt", lowercase = T)
 
-clmet <- train_word2vec("Clmet_processed.txt", 
-                         output = "Clmet_model.bin", threads = 1, 
-                         vectors = 100, window = 12)
+clmet <- train_word2vec("Clmet_processed_period_1_2.txt", 
+                         output = "Clmet_model_period_1_2.bin", threads = 1, 
+                          vectors = 300, window = 12)
 
-independence_250 <- closest_to(clmet, "independence", n = 250)
+clmet <- read.vectors("Clmet_model_period_1_2.bin")
+
+# independence_250 <- closest_to(clmet, "independence", n = 250)
 independence_500 <- closest_to(clmet, "independence", n = 500)
 
-ind_250_names <- independence_250$word
+head(independence_500)
+
+ind_wealth <- closest_to(clmet, ~ "independence" + "wealth", n = 100)
+
+head(ind_wealth)
+
+ind_status <- closest_to(clmet, ~ "independence" + "status", n = 100)
+
+head(ind_status)
+
+ind_thought <- closest_to(clmet, ~ "independence" + "thought", n = 100)
+
+head(ind_thought)
+
+ind_self <- closest_to(clmet, ~ "independence" + "self", n = 100)
+
+head(ind_self)
+
+ind_marriage <- closest_to(clmet, ~ "independence" + "marriage", n = 100)
+
+head(ind_marriage)
+
+ind_nation <- closest_to(clmet, ~ "independence" + "nation", n = 100)
+
+head(ind_nation)
+
 ind_500_names <- independence_500$word
+
